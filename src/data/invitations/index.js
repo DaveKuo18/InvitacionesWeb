a9c610790b18e4f8ed6389571e02ed3d51ef51b1
DaveKuo <davee183@gmail.com>
2026-05-13 16:13:49 -0300

    se sacan las invitaciones que no se usan

--- src/data/invitations/index.js
+++ src/data/invitations/index.js
@@ -1,7 +1,3 @@
-import { me_recibo_magui } from "./me_recibo_magui.js";
-import { valentina15 } from "./valentina-15.js";
 
 export const clientInvitations = [
-  valentina15,
-  me_recibo_magui,
 ];
